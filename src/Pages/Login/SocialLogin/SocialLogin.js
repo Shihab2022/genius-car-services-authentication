b14e398d02b92b7e52917e5944e3567b5a844e3b
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-04-14 15:59:57 +0600

    implement google login

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -2,7 +2,17 @@ import React from "react";
 import google from "../../../images/social/google.png";
 import github from "../../../images/social/github.png";
 import facebook from "../../../images/social/facebook.png";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
+import { useNavigate } from "react-router-dom";
 const SocialLogin = () => {
+
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate=useNavigate();
+if(user){
+navigate('/home')
+}
+
   return (
     <div>
       <div className="d-flex my-3 align-items-center justify-content-center">
@@ -17,16 +27,19 @@ const SocialLogin = () => {
         </div>
       </div>
 
+
+<p className='text-danger fs-5'>{ error?.message ? error?.message : ''}</p>
+<p className='text-info fs-3'>{ loading ? 'Please wait ................' : ''}</p>
       <div>
-        <button className="btn btn-info w-50  d-block mx-auto">
+        <button onClick={()=>signInWithGoogle()} className="btn btn-info w-50  d-block mx-auto">
           <img src={google} alt="" />
           <span className="px-4 fs-5">Google Sign In</span>
         </button>
-        <button className="btn btn-info w-50 my-4 d-block mx-auto">
+        <button className="btn  btn-secondary w-50 my-4 d-block mx-auto">
           <img src={github} alt="" />
           <span className="px-4 fs-5">Github Sign In</span>
         </button>
-        <button className="btn btn-info w-50  d-block mx-auto">
+        <button className="btn btn-warning w-50  d-block mx-auto">
           <img style={{ width: "30px" }} src={facebook} alt="" />
           <span className="px-4 fs-5">Facebook Sign In</span>
         </button>
